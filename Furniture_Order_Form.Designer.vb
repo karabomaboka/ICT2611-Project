ea6dd6f1d8715050a4ebb8b85fd14869814e1675
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2021-11-16 21:18:48 +0200

    Add files via upload

---
+++ Furniture_Order_Form.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Furniture_Order_Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnProcess = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtCity = New System.Windows.Forms.TextBox()
+        Me.txtChairs = New System.Windows.Forms.TextBox()
+        Me.txtSofas = New System.Windows.Forms.TextBox()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(67, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(136, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Customer Name (Last, First)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(127, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Street Address"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(102, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(101, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "City, Sate, Zip Code"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(115, 203)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(88, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Number of Chairs"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(115, 254)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(86, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Number of Sofas"
+        '
+        'btnProcess
+        '
+        Me.btnProcess.AutoSize = True
+        Me.btnProcess.Location = New System.Drawing.Point(71, 329)
+        Me.btnProcess.Name = "btnProcess"
+        Me.btnProcess.Size = New System.Drawing.Size(222, 23)
+        Me.btnProcess.TabIndex = 5
+        Me.btnProcess.Text = "Process Order"
+        Me.btnProcess.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(328, 329)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(275, 23)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(644, 329)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(217, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(225, 39)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(250, 20)
+        Me.txtName.TabIndex = 8
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(225, 94)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(250, 20)
+        Me.txtAddress.TabIndex = 9
+        '
+        'txtCity
+        '
+        Me.txtCity.Location = New System.Drawing.Point(225, 145)
+        Me.txtCity.Name = "txtCity"
+        Me.txtCity.Size = New System.Drawing.Size(250, 20)
+        Me.txtCity.TabIndex = 10
+        '
+        'txtChairs
+        '
+        Me.txtChairs.Location = New System.Drawing.Point(225, 200)
+        Me.txtChairs.Name = "txtChairs"
+        Me.txtChairs.Size = New System.Drawing.Size(57, 20)
+        Me.txtChairs.TabIndex = 11
+        '
+        'txtSofas
+        '
+        Me.txtSofas.Location = New System.Drawing.Point(225, 251)
+        Me.txtSofas.Name = "txtSofas"
+        Me.txtSofas.Size = New System.Drawing.Size(57, 20)
+        Me.txtSofas.TabIndex = 12
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.Location = New System.Drawing.Point(545, 42)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(316, 225)
+        Me.lstDisplay.TabIndex = 13
+        '
+        'Furniture_Order_Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(932, 391)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.txtSofas)
+        Me.Controls.Add(Me.txtChairs)
+        Me.Controls.Add(Me.txtCity)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnProcess)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Furniture_Order_Form"
+        Me.Text = "Furniture Order Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnProcess As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtCity As TextBox
+    Friend WithEvents txtChairs As TextBox
+    Friend WithEvents txtSofas As TextBox
+    Friend WithEvents lstDisplay As ListBox
+End Class
---
+++ Furniture_Order_Form.vb
@@ -0,0 +1,118 @@
+﻿Option Strict On
+
+Public Class Furniture_Order_Form
+    'Declare Constants
+    Const dblCHAIR_PRICE As Double = 350
+    Const dblSOFA_PRICE As Double = 925
+    Const dblSALE_TAX As Double = 0.05
+
+    Private Sub btnProcess_Click(sender As Object, e As EventArgs) Handles btnProcess.Click
+        'Declare variables that will hold the values that are put in the textboxes by the user.
+        Dim strCustName As String = txtName.Text
+        Dim strStreet As String = txtAddress.Text
+        Dim strCity As String = txtCity.Text
+        Dim intChairs As Integer
+        Dim intSofas As Integer
+
+        'Declare variables to calculate the total.
+        Dim dblTotalDue As Double
+        Dim dblTotalTax As Double
+        Dim dblOrderPrice As Double
+
+        Dim strInvoiceId As String
+        Dim strReversedName As String
+
+        'Validate the input into the textboxes.
+        If ValidateInput(strCustName, strStreet, strCity) Then
+            intChairs = CInt(txtChairs.Text)
+            intSofas = CInt(txtSofas.Text)
+
+            dblTotalDue = Calculate(intChairs, intSofas, dblTotalTax, dblOrderPrice)
+
+            strInvoiceId = InvoiceNumber(strCustName, strCity)
+            strReversedName = ReverseName(strCustName)
+
+            Call DisplayBill(intChairs, intSofas, dblOrderPrice, dblTotalTax, dblTotalDue, strInvoiceId, strReversedName, strStreet, strCity)
+        End If
+    End Sub
+
+    'Clear all textboxes and listbox.
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        For Each ctrl In Controls.OfType(Of TextBox)
+            ctrl.Clear()
+        Next
+
+        lstDisplay.Items.Clear()
+        txtName.Focus()
+    End Sub
+
+    'End program.
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    ''''''''''''''''''''''''''''''''SUBPROCEDURES AND FUNCTIONS'''''''''''''''''''''''''''''''''''''
+
+    Private Sub DisplayBill(intChairs As Integer, intSofas As Integer, dblOrderPrice As Double, dblTotalTax As Double, dblTotalDue As Double,
+                            strInvoiceId As String, strReversedName As String, strStreet As String, strCity As String)
+        lstDisplay.Items.Add("Invoice Number: " + strInvoiceId)
+        lstDisplay.Items.Add("")
+        lstDisplay.Items.Add("Name: " + strReversedName)
+        lstDisplay.Items.Add("Street Address: " + strStreet)
+        lstDisplay.Items.Add("City, State, Zip Code: " + strCity)
+        lstDisplay.Items.Add("")
+        lstDisplay.Items.Add("Number of Chairs: " + intChairs.ToString)
+        lstDisplay.Items.Add("Number of Sofas: " + intSofas.ToString)
+        lstDisplay.Items.Add("")
+        lstDisplay.Items.Add("Order Price: " + FormatCurrency(dblOrderPrice))
+        lstDisplay.Items.Add("Sales Tax: " + FormatCurrency(dblTotalTax))
+        lstDisplay.Items.Add("              --------------------")
+        lstDisplay.Items.Add("Total Price: " + FormatCurrency(dblTotalDue))
+    End Sub
+
+
+    Function ValidateInput(strCustName As String, strStreet As String, strCity As String) As Boolean
+        If Not strCustName.Contains(",") Or strCustName.Length < 4 Then
+            MessageBox.Show("Invalid Name. Make sure the Last Name and First Name are separated by a comma", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        ElseIf strStreet = "" Or strCity = "" Then
+            MessageBox.Show("Invalid City info", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        ElseIf Not IsNumeric(txtChairs.Text) Or Not IsNumeric(txtSofas.Text) Then
+            MessageBox.Show("Invalid number of chairs or sofas. Please enter numeric values", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+
+    Function Calculate(intChairs As Integer, intSofas As Integer, ByRef dblTotalTax As Double, ByRef dblOrderPrice As Double) As Double
+        dblOrderPrice = (intChairs * dblCHAIR_PRICE) + (intSofas * dblSOFA_PRICE)
+        dblTotalTax = dblOrderPrice * dblSALE_TAX
+
+        Return dblOrderPrice + dblTotalTax
+    End Function
+
+
+    Function InvoiceNumber(strCustName As String, strCity As String) As String
+        Dim strZipDigits As String
+
+        strZipDigits = strCity.Substring(strCity.Length - 4)
+
+        Return strCustName.Substring(0, 2).ToUpper() & strZipDigits 'Start at the beginning of the string (0) and go forward (2) characters. Convert the characters to uppercase. Concatenate with the zip code digits.
+    End Function
+
+
+    Function ReverseName(strCustName As String) As String
+        Dim strFirstName As String
+        Dim strLastName As String
+        Dim intCommaPosition As Integer
+
+        intCommaPosition = strCustName.IndexOf(",")
+        strFirstName = strCustName.Substring(intCommaPosition + 2)
+        strLastName = strCustName.Substring(0, intCommaPosition)
+
+        Return strFirstName & ", " & strLastName
+    End Function
+End Class
